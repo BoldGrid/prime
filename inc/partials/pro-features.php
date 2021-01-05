e1736430a4904e5d7267b1d99d4cdebc78237bea
James Rosado <jamesros@boldgrid.com>
2021-01-05 13:42:54 -0500

    add featured image

--- inc/partials/pro-features.php
+++ inc/partials/pro-features.php
@@ -18,6 +18,6 @@ if ( ! defined( 'WPINC' ) ) {
 			<?php esc_html_e( 'Upgrade to Crio Pro today to get over 150 additional Customizer controls, Custom Page Headers, and more.', 'prime' ); ?>
 		</p>
 	</div>
-
+	<div class="pro-feature-image" style="background-image:url(<?php echo esc_url( get_template_directory_uri() ) . '/images/welcome/custom-page-headers.png'; ?>)"></div>
 <?php do_action( 'bgtfw_pro_feature_cards' ); ?>
 </div>

eeeab487ade653ecc716e39012bd29630e590f19
James Rosado <jamesros@boldgrid.com>
2022-09-07 11:04:52 -0400

    enable local bgtfw

--- bin/modules/install-bgtfw.js
+++ bin/modules/install-bgtfw.js
@@ -1,12 +1,13 @@
-const got = require( 'got' ),
-	unzip = require( 'unzip-stream' ),
+const got  = require( 'got' ),
+	unzip  = require( 'unzip-stream' ),
 	pkgDir = require( 'pkg-dir' );
-
+	fs     = require( 'fs' );
 module.exports = async ( localBgtfw ) => {
-	console.log( { localBgtfw } );
 	const rootDir = await pkgDir( __dirname );
 	return new Promise( (resolve, reject) => {
-		got.stream( 'https://github.com/BoldGrid/boldgrid-theme-framework/releases/latest/download/bgtfw.zip' )
+		if ( localBgtfw ) {
+			console.log( 'Creating theme from local bgtfw.zip file' );
+			fs.createReadStream('bgtfw.zip' )
 			.pipe( unzip.Extract( { path: 'inc' } ) )
 			.on( 'error', ( err ) => {
 				console.error( err );
@@ -14,5 +15,15 @@ module.exports = async ( localBgtfw ) => {
 			.on( 'finish', () => {
 				resolve();
 			} );
+		} else {
+			got.stream( 'https://github.com/BoldGrid/boldgrid-theme-framework/releases/latest/download/bgtfw.zip' )
+			.pipe( unzip.Extract( { path: 'inc' } ) )
+			.on( 'error', ( err ) => {
+				console.error( err );
+			} )
+			.on( 'finish', () => {
+				resolve();
+			} );
+		}
 	} );
 };

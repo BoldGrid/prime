4340f3eeab4c7314d2e13564471fbd822d2047b4
SitesByYogi <sitesbyyogi@gmail.com>
2022-11-09 17:26:53 -0500

    woocommerce updates

--- woocommerce/product-searchform.php
+++ woocommerce/product-searchform.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The template for displaying product search form
  *
@@ -11,22 +12,18 @@
  * the readme will list any important changes.
  *
  * @see     https://docs.woocommerce.com/document/template-structure/
- * @author  WooThemes
- * @package WooCommerce/Templates
- * @version 3.3.0
+ * @package WooCommerce\Templates
+ * @version 7.0.1
  */
-
 // Exit if accessed directly
-if ( ! defined( 'ABSPATH' ) ) {
+if (!defined('ABSPATH')) {
 	exit;
 }
-
 ?>
-<form role="search" class="form-inline woocommerce-product-search" method="get" id="searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>">
+<form role="search" class="form-inline woocommerce-product-search" method="get" id="searchform" action="<?php echo esc_url(home_url('/')); ?>">
 	<div class="form-group">
-		<label class="screen-reader-text" for="woocommerce-product-search-field-<?php echo isset( $index ) ? absint( $index ) : 0; ?>"><?php esc_html_e( 'Search for:', 'woocommerce' ); ?></label>
-		<input type="search" id="woocommerce-product-search-field-<?php echo isset( $index ) ? absint( $index ) : 0; ?>" class="search-field form-control" placeholder="<?php echo esc_attr__( 'Search products&hellip;', 'woocommerce' ); ?>" value="<?php echo get_search_query(); ?>" name="s" />
+		<label class="screen-reader-text" for="woocommerce-product-search-field-<?php echo isset($index) ? absint($index) : 0; ?>"><?php esc_html_e('Search for:', 'crio'); ?></label>
+		<input type="search" id="woocommerce-product-search-field-<?php echo isset($index) ? absint($index) : 0; ?>" class="search-field form-control" placeholder="<?php echo esc_attr__('Search products&hellip;', 'crio'); ?>" value="<?php echo get_search_query(); ?>" name="s" />
 	</div>
-	<button type="submit" class="button-primary"><span class="fa fa-search"></span><span>&nbsp;<?php echo esc_attr_x( 'Search', 'submit button', 'woocommerce' ); ?></span></button>
+	<button type="submit" class="button-primary"><span class="fa fa-search"></span><span>&nbsp;<?php echo esc_attr_x('Search', 'submit button', 'crio'); ?></span></button>
 	<input type="hidden" name="post_type" value="product" />
-</form>

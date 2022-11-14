b7ae8e73961a4fb148e1df2d2a7ede04a2ac15ac
James Rosado <jamesros@boldgrid.com>
2022-11-14 15:34:06 -0500

    update simple.php

--- woocommerce/single-product/add-to-cart/simple.php
+++ woocommerce/single-product/add-to-cart/simple.php
@@ -11,8 +11,8 @@
  * the readme will list any important changes.
  *
  * @see https://docs.woocommerce.com/document/template-structure/
- * @package WooCommerce/Templates
- * @version 3.4.0
+ * @package WooCommerce\Templates
+ * @version 7.0.1
  */
 
 defined( 'ABSPATH' ) || exit;
@@ -32,16 +32,18 @@ if ( $product->is_in_stock() ) : ?>
 	<form class="cart" action="<?php echo esc_url( apply_filters( 'woocommerce_add_to_cart_form_action', $product->get_permalink() ) ); ?>" method="post" enctype='multipart/form-data'>
 		<?php do_action( 'woocommerce_before_add_to_cart_button' ); ?>
 
-			<?php
-			do_action( 'woocommerce_before_add_to_cart_quantity' );
+		<?php
+		do_action( 'woocommerce_before_add_to_cart_quantity' );
 
-			woocommerce_quantity_input( array(
+		woocommerce_quantity_input(
+			array(
 				'min_value'   => apply_filters( 'woocommerce_quantity_input_min', $product->get_min_purchase_quantity(), $product ),
 				'max_value'   => apply_filters( 'woocommerce_quantity_input_max', $product->get_max_purchase_quantity(), $product ),
 				'input_value' => isset( $_POST['quantity'] ) ? wc_stock_amount( wp_unslash( $_POST['quantity'] ) ) : $product->get_min_purchase_quantity(), // WPCS: CSRF ok, input var ok.
-			) );
+			)
+		);
 
-			do_action( 'woocommerce_after_add_to_cart_quantity' );
+		do_action( 'woocommerce_after_add_to_cart_quantity' );
 		?>
 
 		<button type="submit" name="add-to-cart" value="<?php echo esc_attr( $product->get_id() ); ?>" class="single_add_to_cart_button btn button-primary"><?php echo esc_html( $product->single_add_to_cart_text() ); ?></button>

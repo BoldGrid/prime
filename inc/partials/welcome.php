5d7a077d916652515af115b794a0339984be997a
jamesros <jamesros@boldgrid.com>
2022-11-15 09:40:07 -0500

    Url filters (#151)
    * add extra filters to the welcome page
    
    * fix comment
    
    * simplify boxes.php
    
    * fix indentation and add comments

--- inc/partials/welcome.php
+++ inc/partials/welcome.php
@@ -10,27 +10,88 @@ if ( ! defined( 'WPINC' ) ) {
 	die;
 }
 
+// Add SVGs to the wp_kses filtering.
+$kses_defaults = wp_kses_allowed_html( 'post' );
+
+$svg_args = array(
+	'svg'   => array(
+		'version'             => true,
+		'preserveaspectratio' => true,
+		'viewbox'             => true,
+		'class'               => true,
+		'style'               => true,
+		'width'               => true,
+		'height'              => true,
+	),
+	'g'     => array(
+		'fill'  => true,
+		'style' => true,
+	),
+	'title' => array(
+		'title' => true,
+		'style' => true,
+	),
+	'path'  => array(
+		'xmlns:default' => true,
+		'd'             => true,
+		'fill'          => true,
+		'style'         => true,
+	),
+);
+
+$allowed_tags = array_merge( $kses_defaults, $svg_args );
+
 // URL to our TMG Recommended Plugins page.
 $crio_tgm_url = admin_url( 'admin.php?page=bgtfw-install-plugins' );
 
+// Filters the 'Get Crio Pro' button on the welcome page.
 $premium_url = apply_filters(
 	'bgtfw_premium_url',
 	'https://boldgrid.com/wordpress-themes/crio/?utm_source=Appearance_-_Crio&utm_medium=Button&utm_campaign=Crio_FTP&utm_content=Get_Crio_Pro'
 );
 
+// Filters the 'BoldGrid.com' link in the main panel of the Welcome page.
+$boldgrid_link = apply_filters(
+	'crio_welcome_boldgrid_link',
+	'<a href="https://www.BoldGrid.com/" target="_blank">BoldGrid.com</a>'
+);
+
+// Filters the 'Post and Page Builder' link in the main panel of the Welcome page.
+$boldgrid_ppb_link = apply_filters(
+	'crio_welcome_boldgrid_ppb_link',
+	'<a href="https://www.boldgrid.com/wordpress-page-builder-by-boldgrid/" target="_blank">Post and Page Builder</a>'
+);
+
+$boxes_array = require get_template_directory() . '/inc/partials/boxes.php';
+
+/**
+ * Filters the boxes of the 'Learning Resourcs' section of the welcome page.
+ *
+ * The array of boxes will be a key value array, with the key being the name
+ * of the box, and the value being the html markup for the box. Keep in mind
+ * that the html will later be filtered through wp_kses_post with 'svg' tags
+ * added to the allowed tags.
+ */
+$boxes_array = apply_filters(
+	'crio_welcome_boxes',
+	$boxes_array
+);
+
+$boxes_string = implode( '', array_values( $boxes_array ) );
+
 // URL to customizer with return to the current page.
 global $wp;
-$crio_current_page = add_query_arg( $wp->query_string, '', home_url( $wp->request ) );
+$crio_current_page   = add_query_arg( $wp->query_string, '', home_url( $wp->request ) );
 $crio_customizer_url = esc_url(
 	add_query_arg(
 		array(
-			'url' => urlencode( $crio_current_page ),
+			'url'    => urlencode( $crio_current_page ),
 			'return' => $crio_current_page,
 		),
 		wp_customize_url()
 	)
 );
-$crio_editor_file = 'post-and-page-builder/post-and-page-builder.php';
+$crio_editor_file    = 'post-and-page-builder/post-and-page-builder.php';
 ?>
 
 <div class="wrap about-wrap bgcrio-about-wrap">
@@ -52,15 +113,15 @@ $crio_editor_file = 'post-and-page-builder/post-and-page-builder.php';
 						</p>
 						<?php if ( ! file_exists( WP_PLUGIN_DIR . DIRECTORY_SEPARATOR . $crio_editor_file ) ) : ?>
 							<p>
-								<a href="<?php echo esc_url( $crio_tgm_url ) ?>" class="button button-primary button-hero"><?php esc_html_e( 'Install Post and Page Builder', 'bgtfw' ); ?></a>
+								<a href="<?php echo esc_url( $crio_tgm_url ); ?>" class="button button-primary button-hero"><?php esc_html_e( 'Install Post and Page Builder', 'bgtfw' ); ?></a>
 							</p>
 							<p>
 								<a href="<?php echo esc_url( $crio_customizer_url ); ?>"class="button button-secondary button-hero"><?php esc_html_e( 'Preview and Customize Starter Content', 'bgtfw' ); ?></a>
 							</p>
 
-						<?php elseif ( ! in_array( $crio_editor_file, apply_filters( 'active_plugins', get_option( 'active_plugins' ) ) ) ) : ?>
+						<?php elseif ( ! in_array( $crio_editor_file, apply_filters( 'active_plugins', get_option( 'active_plugins' ) ), true ) ) : ?>
 							<p>
-								<a href="<?php echo esc_url( add_query_arg( 'plugin_status', 'activate', $crio_tgm_url ) ) ?>" class="button button-primary button-hero crio-welcome-button"><?php esc_html_e( 'Activate Post and Page Builder', 'bgtfw' ); ?></a>
+								<a href="<?php echo esc_url( add_query_arg( 'plugin_status', 'activate', $crio_tgm_url ) ); ?>" class="button button-primary button-hero crio-welcome-button"><?php esc_html_e( 'Activate Post and Page Builder', 'bgtfw' ); ?></a>
 							</p>
 							<p>
 								<a href="<?php echo esc_url( $crio_customizer_url ); ?>"class="button button-secondary button-hero crio-welcome-button"><?php esc_html_e( 'Preview and Customize Starter Content', 'bgtfw' ); ?></a>
@@ -78,7 +139,19 @@ $crio_editor_file = 'post-and-page-builder/post-and-page-builder.php';
 					<div class="bgtfw-welcome-panel-column">
 						<h2><?php esc_html_e( 'Crio - The Theme with More', 'bgtfw' ); ?></h2>
 						<p>
-							<?php echo wp_kses_post( __( 'Welcome to BoldGrid Crio! Crio means "I Create" in Portuguese and this is our aim: To give you the most powerful site creation tools you can use! Visit <a href="https://www.BoldGrid.com/" target="_blank">BoldGrid.com</a> to learn about all the resources we offer. Be sure to install the recommended Post and Page Builder. Crio\'s unique Customizer was designed to integrate with the <a href="https://www.boldgrid.com/wordpress-page-builder-by-boldgrid/" target="_blank">Post and Page Builder</a>, so you can easily use your fonts and colors from the visual editor.', 'bgtfw' ) ); ?>
+							<?php
+							printf(
+								wp_kses_post(
+									/* translators: 1: BoldGrid.com Link, 2: BoldGrid.com PPB Link  */
+									__(
+										'Welcome to BoldGrid Crio! Crio means "I Create" in Portuguese and this is our aim: To give you the most powerful site creation tools you can use! Visit %1$s to learn about all the resources we offer. Be sure to install the recommended Post and Page Builder. Crio\'s unique Customizer was designed to integrate with the %2$s, so you can easily use your fonts and colors from the visual editor.',
+										'bgtfw'
+									)
+								),
+								$boldgrid_link,
+								$boldgrid_ppb_link
+							);
+							?>
 						</p>
 					</div>
 					<div class="bgtfw-welcome-panel-column">
@@ -94,48 +167,7 @@ $crio_editor_file = 'post-and-page-builder/post-and-page-builder.php';
 			<div class="bgtfw-welcome-panel-column-container">
 				<h2><?php esc_html_e( 'Learning Resources', 'bgtfw' ); ?></h2>
 				<div class="wrapper">
-					<div class="box a">
-						<div class="learning-item">
-							<div class="learning-image">
-							<svg version="1.2" preserveAspectRatio="none" viewBox="0 0 24 24" class="ng-element" style="opacity: 1; mix-blend-mode: normal; fill: rgb(249, 91, 38); width: 79px; height: 79px;"><g><path xmlns:default="http://www.w3.org/2000/svg" d="M4 6H2v14c0 1.1.9 2 2 2h14v-2H4V6zm16-4H8c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2V4c0-1.1-.9-2-2-2zm-1 9H9V9h10v2zm-4 4H9v-2h6v2zm4-8H9V5h10v2z" style="fill: rgb(249, 91, 38);"></path></g></svg>
-							</div>
-							<div class="learning-text">
-								<h3><?php esc_html_e( 'Crio Documentation and Tutorials', 'bgtfw' ); ?></h3>
-								<p><?php esc_html_e( 'Step by Step tutorials and easy to follow documentation!', 'bgtfw' ); ?></p>
-							</div>
-						</div>
-					</div>
-					<div class="box b">
-						<div class="learning-item">
-							<div class="learning-image">
-								<svg version="1.2" preserveAspectRatio="none" viewBox="0 0 24 24" class="ng-element" style="opacity: 1; mix-blend-mode: normal; fill: rgb(249, 91, 38); width: 79px; height: 79px;"><g><path xmlns:default="http://www.w3.org/2000/svg" d="M20 15.5c-1.25 0-2.45-.2-3.57-.57-.35-.11-.74-.03-1.02.24l-2.2 2.2c-2.83-1.44-5.15-3.75-6.59-6.59l2.2-2.21c.28-.26.36-.65.25-1C8.7 6.45 8.5 5.25 8.5 4c0-.55-.45-1-1-1H4c-.55 0-1 .45-1 1 0 9.39 7.61 17 17 17 .55 0 1-.45 1-1v-3.5c0-.55-.45-1-1-1zM19 12h2c0-4.97-4.03-9-9-9v2c3.87 0 7 3.13 7 7zm-4 0h2c0-2.76-2.24-5-5-5v2c1.66 0 3 1.34 3 3z" style="fill: rgb(249, 91, 38);"></path></g></svg>
-							</div>
-							<div class="learning-text">
-								<h3><?php esc_html_e( '1 on 1 Support', 'bgtfw' ); ?></h3>
-								<p><?php esc_html_e( 'Need help with Crio that you don\'t see in our Docs or user groups? Contact our Support Team (Premium Feature)', 'bgtfw' ); ?></p>
-							</div>
-						</div>
-					</div>
-					<div class="box c">
-						<div class="learning-item">
-							<div class="learning-image">
-								<svg version="1.2" preserveAspectRatio="none" viewBox="0 0 24 24" class="ng-element" style="opacity: 1; mix-blend-mode: normal; fill: rgb(249, 91, 38); width: 82px; height: 82px;"><g><path xmlns:default="http://www.w3.org/2000/svg" d="M16 11c1.66 0 2.99-1.34 2.99-3S17.66 5 16 5c-1.66 0-3 1.34-3 3s1.34 3 3 3zm-8 0c1.66 0 2.99-1.34 2.99-3S9.66 5 8 5C6.34 5 5 6.34 5 8s1.34 3 3 3zm0 2c-2.33 0-7 1.17-7 3.5V19h14v-2.5c0-2.33-4.67-3.5-7-3.5zm8 0c-.29 0-.62.02-.97.05 1.16.84 1.97 1.97 1.97 3.45V19h6v-2.5c0-2.33-4.67-3.5-7-3.5z" style="fill: rgb(249, 91, 38);"></path></g></svg>
-							</div>
-							<div class="learning-text">
-								<h3><?php esc_html_e( 'Team Orange', 'bgtfw' ); ?></h3>
-								<p><?php esc_html_e( 'An active and helpful place to get answers and tips from users like you!', 'bgtfw' ); ?></p>
-							</div>
-						</div>
-					</div>
-					<p class="box">
-						<a href="http://boldgrid.com/support/boldgrid-crio" target="_blank" class="button button-secondary"><?php esc_html_e( 'Browse Docs', 'bgtfw' ); ?></a>
-					</p>
-					<p class="box">
-						<a href="https://www.boldgrid.com/central/account/help/premium" target="_blank" class="button button-secondary"><?php esc_html_e( 'Get Support', 'bgtfw' ); ?></a>
-					</p>
-					<p class="box">
-						<a href="https://www.facebook.com/groups/BGTeamOrange" target="_blank" class="button button-secondary"><?php esc_html_e( 'Join Us', 'bgtfw' ); ?></a>
-					</p>
+					<?php echo wp_kses( $boxes_string, $allowed_tags ); ?>
 				</div>
 			</div>
 		</div>
